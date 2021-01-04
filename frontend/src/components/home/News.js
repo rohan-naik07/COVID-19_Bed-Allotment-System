9e9835c7a67617ec2f7e93c210a4819652e3bd97
Rohan Naik <royalronny12@gmail.com>
2021-01-04 13:09:48 +0530

    Minor Changes

--- frontend/src/components/home/News.js
+++ frontend/src/components/home/News.js
@@ -3,26 +3,73 @@ import axios from 'axios';
 import { Typography, CircularProgress, Container, FormGroup, FormControlLabel, Switch, Card } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import Paper from '@material-ui/core/Paper';
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Divider from "@material-ui/core/Divider";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme=>({
+    card: {
+        maxWidth: 300,
+        margin: "auto",
+        transition: "0.3s",
+        boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+        "&:hover": {
+          boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+        }
+    },
     container : {
         height : '50%'
     },
+  content: {
+    textAlign: "left",
+    padding: theme.spacing.unit * 3
+  },
+  divider: {
+    margin: `${theme.spacing.unit * 3}px 0`
+  },
     paper: {
-        overflowX: "auto",
-        marginBottom: "20px",
-        padding : 10
+        padding : 10,
+        display : 'inline-block'
     },
+  media: {
+    paddingTop: "56.25%"
+  },
     loading : {
         margin: 'auto',
         paddingTop: '15%'
     },
-})
+}))
 
 const NewsItem = props=>{
+    const classes = useStyles();
+    const urltoImage = props.urltoImage;
+    console.log(urltoImage)
     return (
-        <Card backgroundImage={`url(${props.urltoImage})`}>
-           <Typography variant = 'h6'>{props.title}</Typography> 
+        <Card className={classes.card}>
+            <CardMedia
+            className={classes.media}
+           image  ={
+               urltoImage
+              }/>
+            <CardContent className={classes.content}>
+                <Typography
+                    className={"MuiTypography--heading"}
+                    variant={"h6"}
+                    gutterBottom>
+                   {props.title}
+                </Typography>
+                <Typography
+                    className={"MuiTypography--subheading"}
+                    variant={"caption"}>
+                    {props.description}
+                </Typography>
+                <Divider className={classes.divider} light />
+                <Typography
+                    className={"MuiTypography--subheading"}
+                    variant={"h6"}>
+                    {props.publishedAt}
+                </Typography>
+            </CardContent>
         </Card>
     )
 }
@@ -34,15 +81,17 @@ const News = (props) =>{
             <Typography variant='h4' style={{paddingBottom:'10px'}}>
                     Latest News
             </Typography>
+            <Paper className={classes.paper} elevation={2}>
                 {props.news.map(news=>{
                     return ( <NewsItem author={news.author}
                         title ={news.title}
                         description ={news.description}
                         url = {news.url}
-                        urltoImage = {news.urltoImage}
+                        urltoImage = {news.urlToImage}
                         publishedAt = {news.publishedAt}
                         content = {news.content}/>);
                 })}
+            </Paper>
         </Container>
     );
 }
