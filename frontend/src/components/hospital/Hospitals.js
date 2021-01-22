d834fe19c47b3cdc6ba83c10102d325fd13901c9
Rohan Naik <royalronny12@gmail.com>
2021-01-22 13:20:11 +0530

    Minor Changes

--- frontend/src/components/hospital/Hospitals.js
+++ frontend/src/components/hospital/Hospitals.js
@@ -7,6 +7,7 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import MenuBookSharpIcon from '@material-ui/icons/MenuBookSharp';
+import { Paper,Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
     gridList: {
         flexWrap: 'nowrap',
         // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        marginTop : 10,
         transform: 'translateZ(0)',
     },
     icon: {
@@ -35,32 +37,51 @@ const Hospitals = (props) => {
         marginTop : 20
     };
 
-    const [stores, setStores] = React.useState([
-        {latitude: 47.359423, longitude: -122.021071},
-        {latitude: 47.2052192687988, longitude: -121.988426208496},
-        {latitude: 47.6307081, longitude: -122.1434325},
-        {latitude: 47.3084488, longitude: -122.2140121},
-        {latitude: 47.5524695, longitude: -122.0425407}]);
+    const [stores, setStores] = React.useState([]);
     
         const tileData = [{
-                img: 'https://image.shutterstock.com/image-photo/modern-hospital-style-building-260nw-212251981.jpg',
-                title: 'Image',
-                author: 'author',
+                id : '0',
+                img: 'https://lh5.googleusercontent.com/p/AF1QipPH7DnJZLyEjyR9LgTAR3JIeeP-Kr4wTbxPG6yq=w426-h240-k-no',
+                name: 'Deenanath Mangeshkar Hospital and Research Center',
+                contact : '020 4015 1000',
+                website : 'http://www.dmhospital.org/',
+                latitude : '18.50243538786544',
+                longitude : '73.83211347436865',
+                total_beds : 150,
+                beds_availiable : 78
               },
               {
-                img: 'https://image.shutterstock.com/image-photo/modern-hospital-style-building-260nw-212251981.jpg',
-                title: 'Image',
-                author: 'author',
+                id : '1',
+                img: 'https://lh5.googleusercontent.com/p/AF1QipP_Pw30A-Af42rEuxG9sn99NLp4GIYwaEONQO8V=w426-h240-k-no',
+                name: 'Poona Hospital And Research Centre',
+                contact : '020 6609 6000',
+                website : 'http://www.poonahospital.org/',
+                latitude : '18.511038110122588',
+                longitude : '73.8420267818292',
+                total_beds : 76,
+                beds_availiable : 38
               },
               {
+                id : '2',
                 img: 'https://image.shutterstock.com/image-photo/modern-hospital-style-building-260nw-212251981.jpg',
-                title: 'Image',
-                author: 'author',
+                name: 'Global Hospital & Research Institute-Pune',
+                contact : '020 4015 1000',
+                website : 'http://www.globalhospitalpune.com/',
+                latitude : '18.501458243017794',
+                longitude : '73.84141698368317',
+                total_beds : 50,
+                beds_availiable : 40
               },
               {
+                id : '3',
                 img: 'https://image.shutterstock.com/image-photo/modern-hospital-style-building-260nw-212251981.jpg',
-                title: 'Image',
-                author: 'author',
+                name: 'Bois Hospital and Research Center',
+                contact : '020 4015 1000',
+                website : 'http://www.dmhospital.org/',
+                latitude : '18.50243538786578',
+                longitude : '73.83211347436830',
+                total_beds : 100,
+                beds_availiable : 68
             }];
    
     const displayMarkers = () => {
@@ -73,21 +94,45 @@ const Hospitals = (props) => {
         })
     }
 
+    React.useEffect(()=>{
+        let data=[]
+        tileData.forEach(tile=>{
+            data.push({
+                latitude : tile.latitude,
+                longitude : tile.longitude
+            })
+        })
+        setStores(data);
+    },[])
+
     return (
-        <div className={classes.root}>
-         <Grid container>
+        <div>
+         <Grid container >
+         <Grid item xs={6} style={{position: 'relative', height: '50vh',marginBottom:30}}>
+                <Map
+                google={props.google}
+                zoom={8}
+                resetBoundsOnResize={true}>
+                {displayMarkers()}
+                </Map>
+             </Grid>
+             <Grid item xs={6}>
+                 <Paper elevation={2}>
+                 </Paper>
+             </Grid>
+             </Grid>
              <GridList xs = {12} className={classes.gridList} cols={2.5}>
                 {tileData.map((tile) => (
-                <GridListTile key={tile.img}>
-                    <img src={tile.img} alt={tile.title} />
+                <GridListTile key={tile.id} style={{margin:10}}>
+                    <img src={tile.img} alt={tile.name} />
                     <GridListTileBar
-                    title={tile.title}  
+                    title={tile.name}  
                     classes={{
                         root: classes.titleBar,
                         title: classes.title,
                     }}
                     actionIcon={
-                        <IconButton aria-label={`star ${tile.title}`}>
+                        <IconButton aria-label={`star ${tile.name}`}>
                             <MenuBookSharpIcon className={classes.title} />
                         </IconButton>
                     }
@@ -95,24 +140,11 @@ const Hospitals = (props) => {
                 </GridListTile>
                 ))}
             </GridList>
-            <Grid item xs={6}>
-                <Map
-                google={props.google}
-                zoom={8}
-                style={mapStyles}
-                resetBoundsOnResize={true}
-                initialCenter={{ lat: 47.444, lng: -122.176}}>
-                {displayMarkers()}
-                </Map>
-             </Grid>
-         </Grid>
+        
          </div>
     )
 }
 
-/**
- *
- */
 
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyDpKyIYlTBUlZ_g41ql57cabAGpVp_mJQI'
