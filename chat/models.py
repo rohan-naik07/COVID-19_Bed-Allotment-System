201b92617f9ccd4b6ea294fa29f20bf265c3c5c8
pranjal2410 <newalkarpranjal2410.pn@gmail.com>
2021-05-08 18:11:29 +0530

    Added migrations

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,12 @@
+import uuid
+
 from django.db import models
 
 
 # Create your models here.
+from django.utils.text import slugify
+
+
 class Message(models.Model):
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
     text = models.TextField()
@@ -16,6 +21,11 @@ class Chat(models.Model):
     hospital = models.ForeignKey('portal.Hospital', null=False, related_name='Hospital', on_delete=models.CASCADE)
     messages = models.ManyToManyField(Message, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, max_length=8, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(uuid.uuid4())[:8])
+        super(Chat, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.hospital.name
