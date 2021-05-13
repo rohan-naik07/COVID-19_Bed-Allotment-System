3d160a533c2057a5ac19630755891740a7bf559b
pranjal2410 <newalkarpranjal2410.pn@gmail.com>
2021-05-13 14:58:40 +0530

    Added email backend

--- BedAllotmentSystem/settings.py
+++ BedAllotmentSystem/settings.py
@@ -183,6 +183,7 @@ JWT_AUTH = {
     'JWT_AUTH_COOKIE': None,
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alumni.vit18@gmail.com'
