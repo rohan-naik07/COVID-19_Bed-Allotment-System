1a4d764e922ed7da99b0fed0112a5557e8da274a
pranjal2410 <newalkarpranjal2410.pn@gmail.com>
2021-05-14 16:25:12 +0530

    test asgi settings

--- BedAllotmentSystem/asgi.py
+++ BedAllotmentSystem/asgi.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat import routing
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'BedAllotmentSystem.settings'
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
