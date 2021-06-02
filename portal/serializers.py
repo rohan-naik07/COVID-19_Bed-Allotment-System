d015a8a3ce017d91a07c2e33e59a6a504ba40f11
pranjal2410 <newalkarpranjal2410.pn@gmail.com>
2021-06-02 15:24:21 +0530

    Added creation date

--- portal/serializers.py
+++ portal/serializers.py
@@ -11,7 +11,7 @@ class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = ['is_corona_positive', 'is_diabetic', 'is_heart_patient', 'on_medications', 'accepted', 'rejected',
-                  'is_first_dose', 'is_second_dose', 'priority',
+                  'is_first_dose', 'is_second_dose', 'priority', 'applied_date',
                   'user']
 
 
