bd48f7696ceca386bfd06d0b6678e8562db8cb93
pranjal2410 <newalkarpranjal2410.pn@gmail.com>
2021-06-12 01:43:17 +0530

    Updated settings.py

--- BedAllotmentSystem/settings.py
+++ BedAllotmentSystem/settings.py
@@ -198,8 +198,8 @@ JWT_AUTH = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
